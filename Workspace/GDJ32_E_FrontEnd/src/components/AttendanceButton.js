f0accbeb267d8517a6a693e02599c1219d8d88eb
gand2840 <gand2840@gmail.com>
2021-05-25 13:39:15 +0900

    21.05.25 프로튼앤드 작업중

---
+++ Workspace/GDJ32_E_FrontEnd/src/components/AttendanceButton.js
@@ -0,0 +1,17 @@
+import {
+  Box,
+  Button
+} from '@material-ui/core';
+
+const Attendancebutton = () => {
+  <Box>
+    <Button>
+      출근
+    </Button>
+    <Button>
+      퇴근
+    </Button>
+  </Box>;
+};
+
+export default Attendancebutton;
