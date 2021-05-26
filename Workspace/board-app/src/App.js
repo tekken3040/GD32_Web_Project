31bf02321ce65d72680497ac59223d76f17b397c
tekken3040 <tekken3040@naver.com>
2021-05-26 17:40:44 +0900

    21.05.26 김정환
    리액트 게시판 프로젝트 추가

---
+++ Workspace/board-app/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'; 
+import ListBoardComponent from './components/ListBoardComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+
+function App() {
+  return (
+    <div> 
+      <Router>             
+        <HeaderComponent/> 
+          <div className="container">
+            <Switch>       
+              <Route path = "/" exact component = {ListBoardComponent}></Route>
+              <Route path = "/board" component = {ListBoardComponent}></Route>
+            </Switch>
+          </div>
+        <FooterComponent/> 
+      </Router>
+    </div>
+  );
+}
+
+export default App;
