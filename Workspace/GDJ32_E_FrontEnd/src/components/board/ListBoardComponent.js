9777618bd5a1424aadd440712632e8e4abae346c
tekken3040 <tekken3040@naver.com>
2021-06-16 15:31:46 +0900

    21.06.16 김정환
    게시판 뒤로가기 수정 완료

--- Workspace/GDJ32_E_FrontEnd/src/components/board/ListBoardComponent.js
+++ Workspace/GDJ32_E_FrontEnd/src/components/board/ListBoardComponent.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate /* , useLocation */ } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Button, Box } from '@material-ui/core';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
@@ -39,8 +39,9 @@ const PaginationStyle = withStyles({
 
 const ListBoardComponent = () => {
   const history = useNavigate();
-  // const { state } = useLocation();
+  const { state } = useLocation();
   let pNum = 1;
+  const [pageNum, setNum] = useState(1);
   // const [pNum, setNum] = useState(() => {
   //   if (state != null) return state.pIdx;
   //   return 1;
@@ -51,16 +52,17 @@ const ListBoardComponent = () => {
 
   // 페이지가 로딩될 때, 글 목록만 가져오던 것을 , 페이징 객체도 같이 가져오도록 수정
   useEffect(() => {
+    if (state !== null) pNum = state.pIdx;
     // getBoard호출시 페이지 번호 외에 한페이지에 표시할 게시물 수와 총 페이지 수량을 적어줘야함
     BoardService.getBoards(pNum, 10, 10).then((res) => {
-      // setNum(res.data.pagingData.currentPageNum);
+      setNum(res.data.pagingData.currentPageNum);
       pNum = res.data.pagingData.currentPageNum;
       setPage(res.data.pagingData);
       setBoards(res.data.list);
       console.log(res.data.list);
       console.log('pNum : ', pNum);
     });
-  }, [pNum]);
+  }, [state]);
 
   // 지정한 페이지에 해당하는 글목록과 페이지 객체를 가져오는 함수
   // const listBoard = (pageNum) => {
@@ -220,13 +222,13 @@ const ListBoardComponent = () => {
       </Box>
       <Box className={classes.root}>
         <Typography>
-          Page: {pNum}
+          Page: {pageNum}
           <PaginationStyle
             count={paging.pageNumCountTotal}
-            page={pNum}
+            page={pageNum}
             onChange={handleChange}
-            value={pNum}
-            name="pNum"
+            value={pageNum}
+            name="pageNum"
           />
         </Typography>
       </Box>
